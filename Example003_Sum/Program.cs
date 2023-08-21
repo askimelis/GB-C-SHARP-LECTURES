efc57a6ca2438db61c60296500081f3fe31765b5
Arturs <janvaris91@gmail.com>
2023-08-21 19:22:45 +0200

    Amend Example003 to use random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 5;
-int numB = 6;
+﻿int numA = new Random().Next(1, 10); //will choose random number from 1 to 9
+int numB = new Random().Next(1, 10);
 int result = numA + numB; 
+Console.WriteLine("Number A is " + numA);
+Console.WriteLine("Number B is " + numB);
 Console.WriteLine("Sum of A and B is " + result);
