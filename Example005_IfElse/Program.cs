5b090199b053a29be50a564f4791d3bcdd435a9c
Arturs <janvaris91@gmail.com>
2023-08-21 19:30:22 +0200

    Amend Example005 with If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please enter your username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "arturs")
+{
+    Console.WriteLine("Hello, our best Arturs!");
+}
+else
+{
+    Console.WriteLine("Good day, " + username);
+
+}
